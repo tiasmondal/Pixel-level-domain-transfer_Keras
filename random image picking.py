99dca529a73cd96e5e93debac8ca4b7a24e8d33f
tiasmondal <41468297+tiasmondal@users.noreply.github.com>
2019-06-30 09:35:25 +0530

    Random image picking function

---
+++ random image picking.py
@@ -0,0 +1,10 @@
+import random
+
+def random_image_picking_R(set_of_images):
+  number=random.choice([0,1])
+  return(set_of_images[number])
+
+def random_image_picking_A(set_of_images):
+  number=random.choice([0,1,2])
+  return(set_of_images[number])
+
